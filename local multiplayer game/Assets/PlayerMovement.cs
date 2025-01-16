b6b0c15f5aafe2a2853cd714d7895ae404843e01
JillevBeers <410006926@st.roc.a12.nl>
2025-01-16 12:12:38 +0100

    Look desc
    - Coinscript toegevoegd + coin prefab gemaakt
    - Movement gefixt! (we moesten 2 separated is grounded maken)
    - en bezig geweest met de GDD

--- local multiplayer game/Assets/PlayerMovement.cs
+++ local multiplayer game/Assets/PlayerMovement.cs
@@ -9,7 +9,9 @@ public class PlayerMovement : MonoBehaviour
 
     public float movespeed = 10f;
     public float jumpforce = 7f;
-    public bool isGrounded = true;
+    public bool isGroundedP1 = true; // Grounded voor player 1
+    public bool isGroundedP2 = true; // Grounded voor player 2
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,28 +21,32 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //player 1
+        // player 1
         float HorizontalInputp1 = Input.GetAxis("Horizontal");
         player1.velocity = new Vector2(HorizontalInputp1 * movespeed, player1.velocity.y);
-        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.W) && isGroundedP1)
         {
             player1.velocity = new Vector2(player1.velocity.x, jumpforce);
+            isGroundedP1 = false; // Player 1 is niet meer grounded na springen
         }
 
-        //player 2
+        // player 2
         float HorizontalInputp2 = Input.GetAxis("Horizontal2");
         player2.velocity = new Vector2(HorizontalInputp2 * movespeed, player2.velocity.y);
-        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGroundedP2)
         {
             player2.velocity = new Vector2(player2.velocity.x, jumpforce);
+            isGroundedP2 = false; // Player 2 is niet meer grounded na springen
         }
     }
-    //ground test
+
+    // ground test
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Ground"))
         {
-            isGrounded = true;
+            isGroundedP1 = true; // Player 1 is grounded
+            isGroundedP2 = true; // Player 2 is grounded (als beide spelers tegelijkertijd de grond raken)
         }
     }
 
@@ -49,8 +55,11 @@ public class PlayerMovement : MonoBehaviour
     {
         if (collision.collider.CompareTag("Ground"))
         {
-            isGrounded = false;
+            if (collision.gameObject == player1.gameObject)
+                isGroundedP1 = false;
+
+            if (collision.gameObject == player2.gameObject)
+                isGroundedP2 = false;
         }
     }
-
 }
